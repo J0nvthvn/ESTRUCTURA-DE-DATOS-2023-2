f123ad5312d8bdb6e97e8415912b2fad2b8a624f
J0nvthvn <jonathanignacio394@gmail.com>
2023-08-12 14:30:31 -0400

    Create Scanner.java

---
+++ Scanner.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;  
+
+class Io
+{
+    public static void main(String args[])
+    {
+        // Crea un objeto Scanner
+        Scanner stdin = new Scanner(System.in);  
+        System.out.println("Ingresa dos ints:");
+
+        int x = stdin.nextInt();
+        int y = stdin.nextInt();
+
+        System.out.println("el primer numero es: " + x);
+        System.out.println("el segundo numero es: " +  y);
+
+        System.out.println("La suma de los numeros es: " + (x+y));
+        stdin.close();
+    }
+}
