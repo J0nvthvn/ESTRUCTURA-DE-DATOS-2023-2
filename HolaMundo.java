a71a42bcd49a85371416babdf291979cbfc11df0
J0nvthvn <jonathanignacio394@gmail.com>
2023-08-12 15:39:44 -0400

    Hola mundo

--- HolaMundo.java
+++ HolaMundo.java
@@ -1,6 +1,5 @@
 public class HolaMundo {
     public static void main(String[] args) {
-        String word = "Hola mundo!";
-        System.out.println(word);
+        System.out.println("Hola mundo!");
     }
 }
