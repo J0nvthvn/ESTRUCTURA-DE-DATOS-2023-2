52c2c6a8cdb4b735adf4ae1a48f1128e3d3c035c
J0nvthvn <jonathan.flores@mail.udp.cl>
2023-09-04 23:47:49 -0300

    Create CalcularFactorial.java

---
+++ CalcularFactorial.java
@@ -0,0 +1,18 @@
+public class CalcularFactorial {
+    public static void main(String[] args) {
+       int n=5;
+       int resultado=factorial(n);
+       System.out.println(resultado); 
+    }
+
+    public static int factorial(int numero){
+        int res;
+
+        if(numero==1){
+            return 1;
+        }else{
+            res=numero*factorial(numero-1);
+        }
+        return res;
+    }
+}
